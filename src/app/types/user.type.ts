bcc708f4d0e3b1574419c0af15f96163a64ec02f
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-10-02 16:49:45 -0300

    Add files via upload

---
+++ src/app/types/user.type.ts
@@ -0,0 +1,9 @@
+export interface IUser 
+{
+    id: number;
+    username: string;
+    pfp: string;
+    email: string;
+    password: string;
+    description: string;
+}
---
+++ src/environments.ts
@@ -0,0 +1,7 @@
+export const environment = 
+{
+    api_url: 'http://localhost:9654',
+    localstorage_token_key: 'token',
+    localstorage_email_key: 'email',
+    localStorage_user_id: 'user_id',
+}
